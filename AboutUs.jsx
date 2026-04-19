98f9f78778ed16b2bbe551393f782d360ae0b11e
Om Gaikwad <omgaikwad842@gmail.com>
2026-04-19 17:33:38 +0530

    Add AboutUs component with nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Welcome to Paradise Nursery. We provide a variety of beautiful house plants
+        including aromatic and medicinal plants. Our plants help create a healthy
+        and peaceful environment in your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
